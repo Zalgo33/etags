2176918f74ca9507ff4f7a2872fd3e6b6d868e6b
Zalgo <beulers.mays@gmail.com>
2023-10-20 10:41:47 +0200

    Create etags.py

---
+++ etags.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import requests
+
+url = "https://stelycube.fr/p/les-packs"  # Remplacez l'URL par celle de la ressource que vous souhaitez interroger.
+
+# Effectuer une requête HTTP GET pour obtenir la ressource et ses en-têtes
+# Optionnel: verify=False
+response = requests.get(url)
+
+
+# Vérifier si la requête a réussi
+if response.status_code == 200:
+    # L'en-tête ETag est accessible via le dictionnaire des en-têtes de la réponse
+    etag = response.headers.get("ETag")
+    if etag:
+        print(f"ETag de la ressource : {etag}")
+    else:
+        print("Aucun en-tête ETag trouvé.")
+else:
+    print(f"La requête a échoué avec le code d'état {response.status_code}")
